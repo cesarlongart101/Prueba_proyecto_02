bcdbd9549cb940b598841495a8f276c1bf6e1c8d
cesarlongart101 <cesarlongart101@gmail.com>
2023-04-22 12:07:04 +0200

    Inicio Nuevo Programa

---
+++ Ejercicio conjunto.py
@@ -0,0 +1,16 @@
+#Debemos hacer un ejercicio donde Vanessa va a hacer
+# las operaciones matematicas y Cesar va a hacer los mensajes printcipas y los input
+
+
+#Zona Cesar
+print("---------------------------------")
+print("Bienvenido al programa de cálculo")
+print("---------------------------------")
+
+operacion = input ("¿Qué operación quieres hacer? Suma, Resta, Multiplicación o División: ")
+
+if operacion == "Suma":
+    num_1 = input("Dime un número: ")
+    num_2 = input("Dime otro número: ")
+#Zona de Vane
+
