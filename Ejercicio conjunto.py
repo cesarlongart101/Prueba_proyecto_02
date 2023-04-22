60714a5fe69220ad927ca4838e9e22f9c97c38a2
vanefalmiron <vanealmironprogramacion@gmail.com>
2023-04-22 06:15:32 -0400

    agregué suma

--- Ejercicio conjunto.py
+++ Ejercicio conjunto.py
@@ -7,10 +7,17 @@ print("---------------------------------")
 print("Bienvenido al programa de cálculo")
 print("---------------------------------")
 
-operacion = input ("¿Qué operación quieres hacer? Suma, Resta, Multiplicación o División: ")
+operacion = input("¿Qué operación quieres hacer? Suma, Resta, Multiplicación o División: ")
 
 if operacion == "Suma":
-    num_1 = input("Dime un número: ")
-    num_2 = input("Dime otro número: ")
+    num_1 = int(input("Dime un número: "))
+    num_2 = int(input("Dime otro número: "))
 #Zona de Vane
+
+    Suma = num_1 + num_2
+    print("La suma de", num_1, "y", num_2, "es igual a", Suma)
+
+
+
+
 
